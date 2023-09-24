4ec9286bba8548e9e569b5905d932221c7667072
Ismaxis <Ismaxis@github.com>
2023-09-24 17:53:16 +0300

    + 4-lab report & 3-lab images

--- 4-Lab/scripts/chunkSizeTest.py
+++ 4-Lab/scripts/chunkSizeTest.py
@@ -5,12 +5,26 @@ def chunkSize():
     chunks = [1, 2, 4, 8, 16]
     print("\nStatic:")
     for i in chunks:
-        subprocess.run([".\\omp4TestStaticChunkSize.exe", "12",
-                        ".\\images\\16k.pgm", ".\\16k_out.pgm", str(i)])
+        subprocess.run(
+            [
+                ".\\omp4TestStaticChunkSize.exe",
+                "12",
+                ".\\images\\16k.pgm",
+                ".\\16k_out.pgm",
+                str(i),
+            ]
+        )
     print("\nDynamic:")
     for i in chunks:
-        subprocess.run(["..\\omp4TestDynamicChunkSize.exe", "12",
-                        ".\\images\\16k.pgm", ".\\16k_out.pgm", str(i)])
+        subprocess.run(
+            [
+                "..\\omp4TestDynamicChunkSize.exe",
+                "12",
+                ".\\images\\16k.pgm",
+                ".\\16k_out.pgm",
+                str(i),
+            ]
+        )
 
 
 for _ in range(3):
