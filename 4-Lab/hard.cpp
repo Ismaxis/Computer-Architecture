76e666a1fe93006c7bb20b97a209cc36a535de79
Maksim Isaev <ismaxis.v@gmail.com>
2024-01-17 02:49:02 +0300

    Update hard.cpp

--- 4-Lab/hard.cpp
+++ 4-Lab/hard.cpp
@@ -9,13 +9,13 @@ int main(const int argc, const char* argv[])
 {
     if (argc < 4)
     {
-        std::cout << "3 arguments expected, " + std::to_string(argc - 1) + " found\n";
-        return 0;
+        std::cout << "3 arguments expected, " << std::to_string(argc - 1) << " found" << std::endl;
+        return -1;
     }
 
-    const std::string threadsCountStr = argv[1];
-    const std::string in = argv[2];
-    const std::string out = argv[3];
+    std::string threadsCountStr = argv[1];
+    std::string in = argv[2];
+    std::string out = argv[3];
 
     int threadsCount;
     try
@@ -23,15 +23,15 @@ int main(const int argc, const char* argv[])
         threadsCount = std::stoi(threadsCountStr);
         if (threadsCount < -1)
         {
-            std::cout << "Threads count " + std::string(threadsCountStr) +
-                " is invalid.\nValid values:\n\t0: Default number of threads\n      >0: Given number of threads\n       -1: Disable OpenMP (SingleThread)\n";
-            return 0;
+            std::cout << "Threads count " << threadsCountStr <<
+                " is invalid.\nValid values:\n\t0: Default number of threads\n      >0: Given number of threads\n       -1: Disable OpenMP (SingleThread)" << std::endl;
+            return -1;
         }
     }
     catch (std::invalid_argument& e)
     {
-        std::cout << "Threads count should be a number: " << std::string(threadsCountStr) << std::endl;
-        return 0;
+        std::cout << "Threads count should be a number: " << threadsCountStr << std::endl;
+        return -1;
     }
 
     try
@@ -43,24 +43,23 @@ int main(const int argc, const char* argv[])
         {
             omp_set_num_threads(threadsCount);
         }
-        const bool isOmpEnabled = threadsCount != -1;
+        bool isOmpEnabled = threadsCount != -1;
 
-        const double start = omp_get_wtime();
-        const std::vector<int> thresholds = calculateOtsuThresholds(image, isOmpEnabled);
+        double start = omp_get_wtime();
+        std::vector<int> thresholds = calculateOtsuThresholds(image, isOmpEnabled);
         image.applyThresholds(thresholds, isOmpEnabled);
-        const double end = omp_get_wtime();
+        double end = omp_get_wtime();
 
-        printf("Time (%i thread(s)): %g ms\n", threadsCount == 0 ? omp_get_max_threads() : threadsCount,
-               (end - start) * 1000);
+        printf("Time (%i thread(s)): %g ms\n", threadsCount == 0 ? omp_get_max_threads() : threadsCount, (end - start) * 1000);
         printf("%u %u %u\n", thresholds[0], thresholds[1], thresholds[2]);
 
         image.saveToFile(out);
     }
-    catch (std::ios_base::failure& e)
+    catch (const std::ios_base::failure& e)
     {
         std::cout << e.what() << std::endl;
     }
-    catch (std::runtime_error& e)
+    catch (const std::runtime_error& e)
     {
         std::cout << e.what() << std::endl;
     }
